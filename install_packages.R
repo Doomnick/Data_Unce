97b2f41af10015db431dda381fb041ef776acca7
DoomedNick <100158062+DoomedNick@users.noreply.github.com>
2025-04-08 14:25:52 +0200

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,7 +1,6 @@
-# Ujisti se, že existuje user-level knihovna
-user_lib <- Sys.getenv("R_LIBS_USER")
-if (!dir.exists(user_lib)) dir.create(user_lib, recursive = TRUE)
-.libPaths(user_lib)
+main_lib <- .libPaths()[1]
+if (!dir.exists(main_lib)) dir.create(main_lib, recursive = TRUE)
+.libPaths(main_lib)
 
 # Seznam balíčků
 packages <- c(
@@ -9,13 +8,14 @@ packages <- c(
   "officer", "dplyr", "knitr", "tcltk", "lubridate", "progressr", "pacman", "tinytex"
 )
 
+# Instalace chybějících balíčků
 missing <- setdiff(packages, rownames(installed.packages()))
 if (length(missing) > 0) {
   install.packages(missing, dependencies = TRUE)
 }
 
+# Instalace TinyTeX
 if (!requireNamespace("tinytex", quietly = TRUE)) {
   install.packages("tinytex")
 }
-
 tinytex::install_tinytex()
